c74c72d9e5a4df972681f5e1aad5148eef0edbf2
pmbhosale96k <pmbhosale353@gmail.com>
2026-02-26 11:43:31 +0530

    backend first commite

---
+++ spring-backend/interviewer-backend/src/main/java/com/example/interviewer_backend/InterviewerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.interviewer_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewerBackendApplication.class, args);
+	}
+
+}
---
+++ spring-backend/interviewer-backend/src/test/java/com/example/interviewer_backend/InterviewerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.interviewer_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
